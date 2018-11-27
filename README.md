768bda00b6430611e205f4f72a5facbc609b8a0c
tomoya ikeda <tikeda@tikeda.local>
2018-11-27 23:24:32 +0900

    See README.md for example

---
+++ README.md
@@ -0,0 +1,2 @@
+tikeda:Brainfuck tikeda$ g++ --std=c++2a cppinterpreter.cpp -o interpret
+tikeda:Brainfuck tikeda$ ./interpret HelloWorld.bf
---
+++ cppinterpreter.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stack>
+#include <vector>
+#include <iterator>
+
+char data[30000];
+int ptr, loop;
+ 
+int 
+main(int argc, char* argv[]){
+  if(argc < 2) return -1;
+  std::ifstream ifs(argv[1]);
+  if(ifs.fail()) return -1;
+
+  std::istreambuf_iterator<char> it(ifs);
+  std::istreambuf_iterator<char> last;
+  std::string str(it, last);
+  std::string::iterator cursor = str.begin();
+  
+  while(cursor != str.end()){
+    switch(*cursor){
+      case '<' :
+	{
+	  ptr++;
+	  break;
+	}
+      case '>' :
+	{
+	  ptr--;
+	  break;
+	}
+      case '+' :
+	{
+	  data[ptr]++;
+	  break;
+	}
+      case '-' :
+	{
+	  data[ptr]--;
+	  break;
+	}
+      case '.' :
+	{
+	  std::cout << data[ptr];
+	  break;
+	}
+      case ',' :
+	{
+	  std::cin >> data[ptr];
+	  break;
+	}
+      case '[' : 
+	{
+	  if(data[ptr] == 0){
+	    for(cursor++; loop > 0 || *cursor != ']'; cursor++){
+	      if(*cursor == '[')
+		loop++;
+	      if(*cursor == ']')
+		loop--;
+	    }
+	  }
+	  break;
+	}
+      case ']' :
+	{
+	  if(data[ptr] != 0){
+	    for(cursor--; loop > 0 || *cursor != '['; cursor--){
+	      if(*cursor == ']')
+		loop++;
+	      if(*cursor == '[')
+		loop--;
+	    }
+	    cursor--;
+	  }
+	  break;
+	}
+      default :
+	break;	
+    }
+    cursor++;
+  }
+  
+  return 0;
+}
